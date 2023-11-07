f89614ec1e8d866fd83ee7beba5aa7ac12764652
madhushiillesinghe <madhushiillesinghe225@gmail.com>
2023-11-08 01:05:47 +0530

    initial project

--- src/main/java/lk/ijse/project/controller/addProductController.java
+++ src/main/java/lk/ijse/project/controller/addProductController.java
@@ -1,4 +1,100 @@
 package lk.ijse.project.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 public class addProductController {
+
+    @FXML
+    private Button btnadd;
+
+    @FXML
+    private Button btncancel;
+
+    @FXML
+    private Button btncustomer;
+
+    @FXML
+    private Button btndashboard;
+
+    @FXML
+    private Button btnemployee;
+
+    @FXML
+    private Button btnlogout;
+
+    @FXML
+    private Button btnmachine;
+
+    @FXML
+    private Button btnorder;
+
+    @FXML
+    private Button btnproducts;
+
+    @FXML
+    private Button btnsupplier;
+
+    @FXML
+    private TextField txtexpiredate;
+
+    @FXML
+    private TextField txtproductname;
+
+    @FXML
+    private TextField txtquantity;
+
+    @FXML
+    private TextField txtunitprice;
+
+    @FXML
+    void addbtnonaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelbtnonaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void customeronaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dashboardonaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void employeebtnonaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logoutbtnonaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void machinebtnonaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void orderbtnonaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void productsbtnonaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void supplierbtnonaction(ActionEvent event) {
+
+    }
 }
