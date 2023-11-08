dc4aa18c08c415c47a74b6d9857b8cb6495fff09
madhushiillesinghe <madhushiillesinghe225@gmail.com>
2023-11-08 23:17:52 +0530

    initial project

--- src/main/java/lk/ijse/project/controller/addEmployeeController.java
+++ src/main/java/lk/ijse/project/controller/addEmployeeController.java
@@ -1,4 +1,63 @@
 package lk.ijse.project.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import lk.ijse.project.util.Navigation;
+
+import java.io.IOException;
+
 public class addEmployeeController {
+
+    @FXML
+    private Button btnadd;
+
+    @FXML
+    private Button btncancel;
+
+    @FXML
+    private TextField txtcity;
+
+    @FXML
+    private TextField txtcontactno;
+
+    @FXML
+    private TextField txtemail;
+
+    @FXML
+    private TextField txtfirstname;
+
+    @FXML
+    private TextField txtfirstnatxtstreet;
+
+    @FXML
+    private TextField txthouseno;
+
+    @FXML
+    private TextField txtlastname;
+
+    @FXML
+    private TextField txtnicno;
+
+    @FXML
+    private TextField txtpassword;
+
+    @FXML
+    private TextField txtrole;
+
+    @FXML
+    private TextField txtusername;
+
+    @FXML
+    void addbtnonaction(ActionEvent event) throws IOException {
+        Navigation.switchNavigation("employeeForm.fxml",event);
+
+    }
+
+    @FXML
+    void cancelbtnonaction(ActionEvent event) throws IOException {
+        Navigation.switchNavigation("employeeForm.fxml",event);
+    }
+
 }
--- src/main/java/lk/ijse/project/controller/addMachineCotroller.java
+++ src/main/java/lk/ijse/project/controller/addMachineCotroller.java
@@ -1,4 +1,41 @@
 package lk.ijse.project.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import lk.ijse.project.util.Navigation;
+
+import java.io.IOException;
+
 public class addMachineCotroller {
+
+    @FXML
+    private Button btnadd;
+
+    @FXML
+    private Button btncancel;
+
+    @FXML
+    private TextField txtmachinename;
+
+    @FXML
+    private TextField txtmachinetask;
+
+    @FXML
+    private TextField txtperdayrent;
+
+    @FXML
+    private TextField txtquantity;
+
+    @FXML
+    void addbtnonaction(ActionEvent event) throws IOException {
+        Navigation.switchNavigation("machineForm.fxml",event);
+    }
+
+    @FXML
+    void cancelbtnonaction(ActionEvent event) throws IOException {
+        Navigation.switchNavigation("machineForm.fxml",event);
+    }
+
 }
