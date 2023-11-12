ebbdfc9850d29ffdf64fb234350aa4e37b73a45e
madhushiillesinghe <madhushiillesinghe225@gmail.com>
2023-11-13 00:07:29 +0530

    initial project

--- src/main/java/lk/ijse/project/controller/addOrderMachineController.java
+++ src/main/java/lk/ijse/project/controller/addOrderMachineController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import lk.ijse.project.util.Navigation;
 
@@ -12,10 +13,10 @@ import java.io.IOException;
 public class addOrderMachineController {
 
     @FXML
-    private Button btnadd;
+    private Button btncancel;
 
     @FXML
-    private Button btncancel;
+    private Button btnplaceorder;
 
     @FXML
     private ComboBox<?> comboxcustomerid;
@@ -24,16 +25,45 @@ public class addOrderMachineController {
     private ComboBox<?> comboxmachineid;
 
     @FXML
+    private DatePicker datepickdate;
+
+    @FXML
     private TextField txtdaysofkeep;
 
     @FXML
-    void addbtnonaction(ActionEvent event) throws IOException {
-        Navigation.switchNavigation("addMachineOrderForm.fxml",event);
-    }
+    private TextField txtname;
+
+    @FXML
+    private TextField txtorderid;
+
+    @FXML
+    private TextField txtpayid;
+
+    @FXML
+    private TextField txtpaymentmethod;
+
+    @FXML
+    private TextField txtqtyq;
+
+    @FXML
+    private TextField txtrentprice;
+
+    @FXML
+    private TextField txttotalamount;
 
     @FXML
     void cancelbtnonaction(ActionEvent event) throws IOException {
-        Navigation.switchNavigation("addMachineOrderForm.fxml",event);
+    Navigation.switchNavigation("customerMachineOrderForm",event);
+    }
+
+    @FXML
+    void dateonaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void placeorderbtnonaction(ActionEvent event) {
+
     }
 
 }
--- src/main/java/lk/ijse/project/dto/tm/supplierTm.java
+++ src/main/java/lk/ijse/project/dto/tm/supplierTm.java
@@ -1,4 +1,16 @@
 package lk.ijse.project.dto.tm;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class supplierTm {
+    private String Id;
+    private String Name;
+    private String Type;
+    private int Mobile;
+    private String Email;
 }
