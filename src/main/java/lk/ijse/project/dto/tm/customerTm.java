a0463050cc7703fd979055261f99f12ade2c5523
madhushiillesinghe <madhushiillesinghe225@gmail.com>
2023-11-12 23:24:47 +0530

    initial project

--- src/main/java/lk/ijse/project/dto/tm/customerTm.java
+++ src/main/java/lk/ijse/project/dto/tm/customerTm.java
@@ -1,4 +1,17 @@
 package lk.ijse.project.dto.tm;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class customerTm {
+    private String Id;
+    private String Name;
+    private String Address;
+    private String Account;
+    private int Mobile;
+    private String Email;
 }
